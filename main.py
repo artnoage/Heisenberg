34ca03b4039972f958b34437e9306007050a823a
artnoage <artnoage@gmail.com>
2024-02-20 18:48:16 +0100

    upload

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ import time
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 epsilon=0.00001
-s_values = torch.rand(100).to(device)
+s_values = torch.rand(25).to(device)
 
-v_values = torch.rand(100).to(device)*(2*torch.pi-epsilon) - 2*torch.pi + epsilon/2 
+v_values = torch.rand(25).to(device)*(2*torch.pi-epsilon) - 2*torch.pi + epsilon/2 
 
-theta_values = torch.rand(100).to(device)*2*torch.pi  # 45 degrees in radians
+theta_values = torch.rand(25).to(device)*2*torch.pi  # 45 degrees in radians
 
-r_values = torch.rand(100).to(device) # r should be positive
+r_values = torch.rand(25).to(device) # r should be positive
 
  # 100 random points for s, scaled for a wider range
 # Create a tensor of shape [100, 4] for spherical coordinates with the same v, theta, r
 
 spherical_coords = torch.cartesian_prod(s_values, v_values, theta_values, r_values)
-print(spherical_coords.shape())
-test=spherical_coords[:,3]*spherical_coords[:,4]
+print(spherical_coords.shape)
+test=spherical_coords[:,0]*spherical_coords[:,3]
 # Convert to Cartesian coordinates
 if  True:
     cartesian_coords = spherical_to_cartesian(spherical_coords)
     norm=norm_cc(cartesian_coords)
-    print(torch.max(torch.abs(norm-s_values)))
+    print(torch.max(torch.abs(norm-test)))
 
 
 if False:
